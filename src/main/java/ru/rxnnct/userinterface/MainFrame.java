18a8a7e19a76eadf84bf5cd0787e9bb3de721fda
Rixen <rixennoct@rambler.ru>
2019-06-10 00:51:14 +0300

    MainFrame fixes.

--- src/main/java/ru/rxnnct/userinterface/MainFrame.java
+++ src/main/java/ru/rxnnct/userinterface/MainFrame.java
@@ -7,14 +7,19 @@ class MainFrame {
     private String FRAME_TITLE = "The Endless Journey";
     private int FRAME_WIDTH = 800;
     private int FRAME_HEIGHT = 600;
+
     private JFrame frame;
 
     //constructor
     MainFrame() {
+        setFrame();
+    }
+
+    private void setFrame(){
         this.frame = new JFrame(FRAME_TITLE);
         this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setLayout(new BorderLayout(1, 1));
+        //this.frame.setLayout(new BorderLayout(1, 1));
         this.frame.setResizable(false);
         this.frame.setVisible(true);
     }
