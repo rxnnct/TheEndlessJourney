8d6c8e1fe72da898f094402e6f7591294f2820b1
Rixen <rixennoct@rambler.ru>
2019-08-24 22:35:59 +0300

    Help UI changes.

--- src/main/java/ru/rxnnct/userinterface/Help.java
+++ src/main/java/ru/rxnnct/userinterface/Help.java
@@ -7,28 +7,29 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Help extends JComponent {
+    private final int TEXT_LEFT_MARGIN = 30;
 
     public void paint(Graphics graphics) {
         if (Application.getInstance().getApplicationState() == ApplicationStates.HELP) {
             Graphics2D graphics2d = (Graphics2D) graphics;
 
-            //Текст:
+            //Text
             graphics2d.setPaint(Color.black);
             Font currentFont = graphics2d.getFont();
             graphics2d.setFont(currentFont.deriveFont(currentFont.getSize() * 1.6F));
-            graphics2d.drawString("Управление на карте: стрелки", 30, 25);
-            graphics2d.drawString("Управление в бою:", 30, 75);
-            graphics2d.drawString("Q: Атака", 30, 100);
-            graphics2d.drawString("W: Проникающая атака (игнорирует блок)", 30, 125);
-            graphics2d.drawString("E: Жар (враг обжигается, когда атакует)", 30, 150);
-            graphics2d.drawString("A: Блок (блокирует все, кроме вытягивания и проникающей атаки)", 30, 175);
-            graphics2d.drawString("S: Вытягивание здоровья (крадет часть здоровья)", 30, 200);
-            graphics2d.drawString("D: Исцеление", 30, 225);
-            graphics2d.drawString("Противники обладают различным поведением во время, которое зависит", 30, 300);
-            graphics2d.drawString("от типа его владельца. На каждом следующем уровне здоровье противников", 30, 325);
-            graphics2d.drawString("увеличивается, а ваше - восстанавливается. Кроме того, количество", 30, 350);
-            graphics2d.drawString("очков за победы с каждым уровнем растет. Учитывайте это во время путешествия.", 30, 375);
-            graphics2d.drawString("ESC: выйти в меню", 30, 525);
+            graphics2d.drawString("Map controls: arrow keys", TEXT_LEFT_MARGIN, 25);
+            graphics2d.drawString("Battle controls:", TEXT_LEFT_MARGIN, 75);
+            graphics2d.drawString("Q: Simple attack spell", TEXT_LEFT_MARGIN, 100);
+            graphics2d.drawString("W: Piercing attack spell (ignores blocking)", TEXT_LEFT_MARGIN, 125);
+            graphics2d.drawString("E: Attack spell with self-burning (the enemy burns when he attacks)", TEXT_LEFT_MARGIN, 150);
+            graphics2d.drawString("A: Blocking spell", TEXT_LEFT_MARGIN, 175);
+            graphics2d.drawString("S: Health drain (ignores blocking and drains health)", TEXT_LEFT_MARGIN, 200);
+            graphics2d.drawString("D: Heal", TEXT_LEFT_MARGIN, 225);
+            graphics2d.drawString("Противники обладают различным поведением во время, которое зависит", TEXT_LEFT_MARGIN, 300);
+            graphics2d.drawString("от типа его владельца. На каждом следующем уровне здоровье противников", TEXT_LEFT_MARGIN, 325);
+            graphics2d.drawString("увеличивается, а ваше - восстанавливается. Кроме того, количество", TEXT_LEFT_MARGIN, 350);
+            graphics2d.drawString("очков за победы с каждым уровнем растет. Учитывайте это во время путешествия.", TEXT_LEFT_MARGIN, 375);
+            graphics2d.drawString("ESC: exit to main men.", TEXT_LEFT_MARGIN, 525);
 
             super.repaint();
         }
