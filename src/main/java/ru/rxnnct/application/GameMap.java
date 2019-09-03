9c93cb18a9009f92c8effcad9fde2fe81582dfd6
Rixen <rixennoct@rambler.ru>
2019-09-03 22:10:15 +0300

    MAP_FIELD_EXIT bug fix.

--- src/main/java/ru/rxnnct/application/GameMap.java
+++ src/main/java/ru/rxnnct/application/GameMap.java
@@ -87,14 +87,6 @@ public class GameMap {
             }
             corridorsAcrossLineCounter++;
         }
-        //begin test output -------
-        for (int row = 0; row < 2; row++){
-            for (int col = 0; col < 2; col++){
-                System.out.print(endsArray[row][col] + " ");
-            }
-            System.out.println();
-        }
-        //end
     }
 
     private void createStartAndEndPositions(){
