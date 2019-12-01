483139aedb19231f2aa98601b91ca8bcdc166f7f
Rixen <rixennoct@rambler.ru>
2019-12-01 22:23:55 +0300

    Skills class changes.

--- src/main/java/ru/rxnnct/application/GameCharacter.java
+++ src/main/java/ru/rxnnct/application/GameCharacter.java
@@ -5,6 +5,22 @@ public class GameCharacter {
     protected int currentHitPoints;
     protected int characterPositionRow;
     protected int characterPositionCol;
+    protected Skills hit;
+    protected Skills block;
+    protected Skills heal;
+    protected Skills leech;
+    protected Skills penetrate;
+    protected Skills reflect;
+
+    //constructors
+    public GameCharacter() {
+        hit = new Skills(0, 0, 0 , 2000, 0, 300, 0, 250, false);
+        block = new Skills(0, 0, 0 , 1500, 700, 0, 0, 0, false);
+        heal = new Skills(0, 0, 0 , 4000, 0, 1000, 500, 0, false);
+        leech = new Skills(0, 0, 0 , 3000, 0, 400, 150, 150, true);
+        penetrate = new Skills(0, 0, 0 , 4000, 0, 200, 0, 200, true);
+        reflect = new Skills(0, 1500, 150 , 5000, 0, 1500, 0, 150, false);
+    }
 
     //accessors
     public int getCurrentHitPoints() {
@@ -33,26 +49,4 @@ public class GameCharacter {
 
     //methods
 
-    public void useSkill(Skills skill, GameCharacter targetGameCharacter){
-        switch (skill){
-            case HIT:
-                System.out.println(this.hashCode() + " " + skill + " " + "Target " + targetGameCharacter.hashCode());
-                break;
-            case BLOCK:
-                System.out.println(this.hashCode() + " " + skill + " " + "Target " + targetGameCharacter.hashCode());
-                break;
-            case HEAL:
-                System.out.println(this.hashCode() + " " + skill + " " + "Target " + targetGameCharacter.hashCode());
-                break;
-            case LEECH:
-                System.out.println(this.hashCode() + " " + skill + " " + "Target " + targetGameCharacter.hashCode());
-                break;
-            case PENETRATE:
-                System.out.println(this.hashCode() + " " + skill + " " + "Target " + targetGameCharacter.hashCode());
-                break;
-            case REFLECT:
-                System.out.println(this.hashCode() + " " + skill + " " + "Target " + targetGameCharacter.hashCode());
-                break;
-        }
-    }
 }
